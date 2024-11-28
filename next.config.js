68bf7374e0ca1e82f7e6ff306c67d1064e9501e9
anm7 <nsite2014@gmail.com>
2024-11-28 17:23:11 +0200

    img

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "data.powweb.space",
+          },
+          {
+            protocol: "https",
+            hostname: "powweb.space",
+          },
+          {
+            protocol: "https",
+            hostname: "i0.wp.com",
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
